568df2ab2afa54b8d557f25fcefd825abb63a963
jayparkeshpandit971-art <jayparkeshpandit971@gmail.com>
2026-05-13 16:33:25 +0545

    Update bot.js

--- bot.js
+++ bot.js
@@ -233,3 +233,8 @@ bot.onText(/\/clearwarn (\d+)/, (msg, match)=>{
 });
 
 console.log("🤖 AI Studio Bot Started!");
+const http = require('http');
+http.createServer((req, res) => {
+  res.write('Bot is alive!');
+  res.end();
+}).listen(process.env.PORT || 3000);
